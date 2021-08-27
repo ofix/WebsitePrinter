6c7a4965b5c3a8e278593c5003d908290cfc7caf
songhuabiao <songhuabiao@greatwall.com.cn>
2021-08-27 11:34:14 +0800

    移除不存在的函数调用

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const websitePrinter = require('./WebsitePrinter');
 let printer = new websitePrinter('https://en.cppreference.com/w/cpp', 'cppreference', 'https://en.cppreference.com');
-let container = [
-    "#cpp-content-base",
-]
 let dirtyElements = [
-    "#mw-head","#cpp-footer-base",".printfooter"
+    ".printfooter"
 ]
-printer.setLevelLinks(levelLinks);
+printer.setDirtyGlobalNodes(dirtyElements);
 printer.build();
